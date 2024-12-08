9f75d8b8881446e5ab458798e5a436675ba41769
tata <tata@example.com>
2024-12-08 18:41:53 +0700

    komit pertama

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $pageTitle }}</title>
+    @vite('resources/sass/app.scss')
+</head>
+<body>
+    @include('layouts.nav')
+    @yield('content')
+    @vite('resources/js/app.js')
+</body>
+</html>
+
